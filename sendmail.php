f090fe744eabef049d494941bdbdac2ed84c29d7
sergcode <shsv100@ya.ru>
2023-02-24 21:34:29 +0700

    change the path of the sendmail.php and bundel.js

--- sendmail.php
+++ sendmail.php
@@ -5,7 +5,7 @@
 
     $message = ' <p>Текст письма</p> </br> <b>1-ая строчка </b> </br><i>2-ая строчка </i> </br>';
 
-    $headers  = "Content-type: text/html; charset=windows-1251 \r\n";
+    $headers  = "Content-type: text/html; charset=UTF-8 \r\n";
     $headers .= "From: От кого письмо <from@example.com>\r\n";
     $headers .= "Reply-To: reply-to@example.com\r\n";
 
