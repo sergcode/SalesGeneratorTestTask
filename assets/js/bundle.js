b3b5788e4ae6981e0ba68e11d5889ea3876b0966
sergcode <shsv100@ya.ru>
2023-02-24 19:33:31 +0700

    change the path of the sendmail.php

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -11247,7 +11247,7 @@ function forms(formSelector) {
 		if (error === 0) {
 			form.classList.add('_sending');
 
-			let response = await fetch('sendmail.php', {
+			let response = await fetch('../sendmail.php', {
 				method: 'POST',
 				body: formData
 			});
